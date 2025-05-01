bedf962702d71a8754937e1e6cfcfb58ae0806c1
szantonik <szantonik@gmail.com>
2025-05-01 10:57:17 +0200

    Fonts, logos & Tailwindcss added

---
+++ EduPath_frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-9xl font-kblack text-pink-400'>Home</div>
+  )
+}
+
+export default Home
---
+++ EduPath_frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+          kregular: ['Kanit-Regular', 'sans-serif'],
+          kblack: ['Kanit-Black', 'sans-serif'],
+          kbold: ['Kanit-Bold', 'sans-serif'],
+          kbolditalic: ['Kanit-BoldItalic', 'sans-serif'],
+          kextralight: ['Kanit-ExtraLight', 'sans-serif'],
+          kextralightitalic: ['Kanit-ExtraLightItalic', 'sans-serif'],
+          kitalic: ['Kanit-Italic', 'sans-serif'],
+          klight: ['Kanit-Light', 'sans-serif'],
+          klightitalic: ['Kanit-LightItalic', 'sans-serif'],
+          kmedium: ['Kanit-Medium', 'sans-serif'],
+          kmediumitalic: ['Kanit-MediumItalic', 'sans-serif'],
+          ksemibold: ['Kanit-SemiBold', 'sans-serif'],
+          ksemibolditalic: ['Kanit-SemiBoldItalic', 'sans-serif'],
+          kthin: ['Kanit-Thin', 'sans-serif'],
+          kthinitalic: ['Kanit-ThinItalic', 'sans-serif'],
+          kextrabold: ['Kanit-ExtraBold', 'sans-serif'],
+          kextrabolditalic: ['Kanit-ExtraBoldItalic', 'sans-serif'],
+          kblackitalic: ['Kanit-BlackItalic', 'sans-serif'],
+        },
+        extend: {
+      },
+    },
+    plugins: [],
+  }  
